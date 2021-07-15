bc965a272a688df5980ecc6705a7ac11b0f5db78
Taavi Raudkiv <taaviraudkivi2@gmail.com>
2021-07-15 13:54:55 +0300

    Fixed mobile view image carousel sizing

--- src/components/ImgPage/ImgSliderElements.js
+++ src/components/ImgPage/ImgSliderElements.js
@@ -6,6 +6,10 @@ export const CarouselContainer = styled.div`
   width: 100%;
   height: 100%;
   background: #d3bc5f;
+
+  @media screen and (max-width: 770px) {
+    margin-top: -1px;
+  }
 `;
 
 export const Header = styled.h1`
@@ -14,6 +18,10 @@ export const Header = styled.h1`
   text-shadow: 2px 2px #000;
   padding-top: 150px;
   text-align: center;
+
+  @media screen and (max-width: 770px) {
+    padding-top: 100px;
+  }
 `;
 
 export const CarouselWrapper = styled.div`
@@ -24,6 +32,11 @@ export const CarouselWrapper = styled.div`
   position: relative;
   z-index: 1;
   margin-top: -100px;
+
+  @media screen and (max-width: 770px) {
+    margin-top: -180px;
+    padding-bottom: -100px;
+  }
 `;
 
 export const Carousel = styled.section`
@@ -41,13 +54,8 @@ export const Images = styled.img`
   border-radius: 10px;
 
   @media screen and (max-width: 770px) {
-    width: 250px;
-    height: 250px;
-  }
-
-  @media screen and (max-width: 460px) {
-    width: 170px;
-    height: 170px;
+    width: 90vw;
+    height: auto;
   }
 `;
 
@@ -60,6 +68,13 @@ export const FaArrowRight = styled(FaAngleRight)`
   z-index: 10;
   cursor: pointer;
   user-select: none;
+
+  @media screen and (max-width: 770px) {
+    top: 20%;
+    right: -10px;
+    background-color: rgba(255, 255, 255, 0.7);
+    font-size: 2.3rem;
+  }
 `;
 
 export const FaArrowLeft = styled(FaAngleLeft)`
@@ -71,6 +86,13 @@ export const FaArrowLeft = styled(FaAngleLeft)`
   z-index: 10;
   cursor: pointer;
   user-select: none;
+
+  @media screen and (max-width: 770px) {
+    top: 20%;
+    left: -10px;
+    background-color: rgba(255, 255, 255, 0.7);
+    font-size: 2.3rem;
+  }
 `;
 
 export const Slide = styled.div`
