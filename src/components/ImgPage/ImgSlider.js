4ec22edb2590cad9ec100982cdcfe749bfb5809d
Taavi Raudkiv <taaviraudkivi2@gmail.com>
2021-09-04 13:55:25 +0300

    fixed img slide and added email validation (without display)

--- src/components/ImgPage/ImgSlider.js
+++ src/components/ImgPage/ImgSlider.js
@@ -17,6 +17,7 @@ import valley1 from "../../images/valley1.png";
 
 const ImgSlider = ({ slides }) => {
   const [current, setCurrent] = useState(0);
+  const [current1, setCurrent1] = useState(0);
   const length = slides.length;
 
   const nextSlide = () => {
@@ -27,52 +28,62 @@ const ImgSlider = ({ slides }) => {
     setCurrent(current === 0 ? length - 1 : current - 1);
   };
 
+  const nextSlide1 = () => {
+    setCurrent1(current1 === length - 1 ? 0 : current1 + 1);
+  };
+
+  const prevSlide1 = () => {
+    setCurrent1(current1 === 0 ? length - 1 : current1 - 1);
+  };
+
   if (!Array.isArray(slides) || slides.length <= 0) {
     return null;
   }
 
   return (
-    <CarouselContainer id="jobs-pics">
-      <Header>TEHTUD TÖÖD</Header>
-      <CarouselWrapper>
-        <Carousel>
-          <FaArrowLeft onClick={prevSlide} />
-          <FaArrowRight onClick={nextSlide} />
-          {SliderData.map((slide, index) => {
-            return (
-              <div
-                className={index === current ? <SlideActive /> : <Slide />}
-                key={index}
-              >
-                {index === current && (
-                  <Images src={slide.image} alt="metsatood" />
-                )}
-              </div>
-            );
-          })}
-        </Carousel>
-      </CarouselWrapper>
-      <Header>MASINAPARK</Header>
-      <CarouselWrapper>
-        <Carousel>
-          <FaArrowLeft onClick={prevSlide} />
-          <FaArrowRight onClick={nextSlide} />
-          {SliderDataMachines.map((slide, index) => {
-            return (
-              <div
-                className={index === current ? <SlideActive /> : <Slide />}
-                key={index}
-              >
-                {index === current && (
-                  <Images src={slide.image} alt="metsatood" />
-                )}
-              </div>
-            );
-          })}
-        </Carousel>
-      </CarouselWrapper>
-      <BottomImage src={valley1} />
-    </CarouselContainer>
+    <section>
+      <CarouselContainer id="jobs-pics">
+        <Header>TEHTUD TÖÖD</Header>
+        <CarouselWrapper>
+          <Carousel>
+            <FaArrowLeft onClick={prevSlide} />
+            <FaArrowRight onClick={nextSlide} />
+            {SliderData.map((slide, index) => {
+              return (
+                <div
+                  className={index === current ? "slide-active" : "slide"}
+                  key={index}
+                >
+                  {index === current && (
+                    <Images src={slide.image} alt="metsatood" />
+                  )}
+                </div>
+              );
+            })}
+          </Carousel>
+        </CarouselWrapper>
+        <Header>MASINAPARK</Header>
+        <CarouselWrapper>
+          <Carousel>
+            <FaArrowLeft onClick={prevSlide1} />
+            <FaArrowRight onClick={nextSlide1} />
+            {SliderDataMachines.map((slide, index) => {
+              return (
+                <div
+                  className={index === current1 ? "slide-active" : "slide"}
+                  key={index}
+                >
+                  {index === current1 && (
+                    <Images src={slide.image} alt="metsatood" />
+                  )}
+                </div>
+              );
+            })}
+          </Carousel>
+        </CarouselWrapper>
+        <BottomImage src={valley1} />
+      </CarouselContainer>
+    </section>
   );
 };
 
---
+++ src/components/Validations/UserValidation.js
@@ -0,0 +1,10 @@
+import * as yup from "yup";
+
+export const userSchema = yup.object().shape({
+  name: yup.string().required("Kohustuslik"),
+  email: yup.string().email().required("Kohustuslik"),
+  text: yup
+    .string()
+    .max(10, "Teksti pikkus vähemalt 10 tähemärki")
+    .required("Kohustuslik"),
+});
